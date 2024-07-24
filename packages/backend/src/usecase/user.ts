c07d22190e35e31f3681aba836de180fb6971f2e
54vio <savio.document@gmail.com>
2024-07-24 11:22:11 -0300

    feat: add create use usecase

--- packages/backend/src/usecase/user.ts
+++ packages/backend/src/usecase/user.ts
@@ -1,6 +1,7 @@
 import bcrypt from 'bcrypt'
 import { UserRepository } from "../repository/user"
 import jwt from "jsonwebtoken"
+import { UserDto } from 'src/dtos/user';
 
 
 export const UserUseCases = {
@@ -17,7 +18,7 @@ export const UserUseCases = {
       if (!passwordMatch) {
         return new Error('Senha ou Email incorretos');
       }
-      
+
       const token = jwt.sign({ userId: _id }, 'secret-key', { expiresIn: '24h' });
 
       return {
@@ -35,6 +36,34 @@ export const UserUseCases = {
     }
   },
 
+  create: async (data : {user : UserDto}) => {
+    try {
+      const userAlreadyExists = await UserRepository.findByEmail(data.user.email);
+
+      if (userAlreadyExists) {
+       return new Error('Email Já em uso, tente outro!');
+      }
+
+      const passwordHash = await bcrypt.hash(data.user.password,8);
+
+      data.user.password = passwordHash;
+
+     
+      const {name,email,company} = await UserRepository.createUser({...data.user});
+
+
+      return {
+        user: {
+          name,
+          email,
+          company,
+        }
+      };
+    } catch (e) {
+      console.log(e)
+      return new Error("Ops! Erro em nossos servidores")
+    }
+  }
 
 }
 
