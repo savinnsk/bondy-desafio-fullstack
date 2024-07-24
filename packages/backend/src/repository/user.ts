4a8be16e76e26da4ae036c51dea21dbbbbc36e07
54vio <savio.document@gmail.com>
2024-07-24 08:50:46 -0300

    feat: add user repository

---
+++ packages/backend/src/repository/user.ts
@@ -0,0 +1,42 @@
+//import { UserDto } from '../dtos/user'
+import { User } from '../models/User'
+
+
+
+export const UserRepository = {
+ 
+  findUserById : async (id : string) : Promise<UserDto> => {
+    try{
+      const user = await User.findById(id)
+  
+      return  user
+  
+    }catch(e : any){
+      throw new Error(e.message || e )
+    }
+  },
+
+  findByEmail : async (email : string) : Promise<UserDto> =>{
+    try{
+      const user = await User.find({email : email})
+  
+      return user[0]
+      
+    }catch(e : any){
+      throw new Error(e.message || e )
+    }
+  },
+
+  createUser : async (data : UserDto) : Promise<UserDto> =>{
+    try{
+      const user = await User.create({...data})
+
+     return user
+
+    }catch(e : any){
+      throw new Error(e.message || e )
+    }
+  }
+
+}
+
